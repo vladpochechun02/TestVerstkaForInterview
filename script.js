9860b938e317108513d81fd2c64c34c4ac085848
Vladyslav <kpigoodvlad@gmail.com>
2023-12-09 23:49:38 +0200

    Finished responsive version

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 'use strict'
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const burger = document.querySelector('.burger-icon');
+    const overlay = document.querySelector('.overlay');
+    const dashboard = document.querySelector('aside');
+
+    burger.addEventListener('click', () => {
+        dashboard.classList.toggle('show');
+        overlay.classList.toggle('show');
+        burger.classList.toggle('show');
+    })
+
+    overlay.addEventListener('click', () => {
+        dashboard.classList.remove('show');
+        overlay.classList.remove('show');
+        burger.classList.remove('show');
+    })
 })
